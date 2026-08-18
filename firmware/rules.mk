d323f7401276a8d98d596a168e050b1817262da1
Aquamangos <105576410+Aquamangos@users.noreply.github.com>
2026-08-17 20:50:11 -0700

    Add files via upload

---
+++ firmware/rules.mk
@@ -0,0 +1,11 @@
+OLED_ENABLE = yes
+OLDEN_TRANSPORT = i2c
+# OLED_DRIVER = vendor
+
+RGBLIGHT_ENABLE = yes
+WS2812_DRIVER = vendor
+
+ENCODER_ENABLE = yes
+
+# LTO is required for rp2040 so dont remove
+LTO_ENABLE = yes
